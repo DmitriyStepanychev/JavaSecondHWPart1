fc81ab6f01b1bca51a1416de0b5d8a3277b2bd5b
Dmitriy Stepanychev <mabcroshi@gmail.com>
2023-02-07 21:41:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    int ticket = 13_676;
+    int bonus = 20;
+    int bonusMiles;
+
+        bonusMiles = ticket / bonus;
+
+        System.out.println("Кол-во начисленных миль: " + bonusMiles);
+    }
+}
