597b81184d86a71c359905982aa6fef1ec74a0a4
Dmitriy Stepanychev <mabcroshi@gmail.com>
2023-02-09 06:20:06 +0300

    Reformating code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    int ticket = 13_676;
-    int bonus = 20;
-    int bonusMiles;
+        int ticket = 13_676;
+        int bonus = 20;
+        int bonusMiles;
 
         bonusMiles = ticket / bonus;
 
