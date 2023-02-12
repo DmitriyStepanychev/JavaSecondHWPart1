9e8ea2ea96826781f34284920fb602d135d9b902
Dmitriy Stepanychev <mabcroshi@gmail.com>
2023-02-12 23:00:23 +0300

    Added service

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 13_676;
-        int bonus = 20;
-        int bonusMiles;
-
-        bonusMiles = ticket / bonus;
-
-        System.out.println("Кол-во начисленных миль: " + bonusMiles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
